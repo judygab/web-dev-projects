fb237b26e26854627aa2fd6934811bb6c6b38ed0
judygab <ivditigabeskiria@gmail.com>
2022-05-30 11:38:24 -0400

    added prev and next btns

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { MultiStepProgressBar } from "./components/MultiStepProgressBar";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { useState } from "react";
 
 function App() {
   const [index, setIndex] = useState(1);
+
+  const prevButton = () => {
+    if (index > 1) {
+      setIndex(prevIndex => prevIndex - 1);
+    }
+  }
+
+  const nextButton = () => {
+    if (index - 3) {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
 
   return (
     <div className="App">
@@ -16,6 +28,14 @@ function App() {
               step={index}
               />
           </Col>
+        </Row>
+        <Row>
+          <Card>
+            <Card.Footer className="d-flex justify-content-between">
+              <Button onClick={prevButton} disabled={index == 1}>Previous</Button>
+              <Button onClick={nextButton}>Next</Button>
+            </Card.Footer>
+          </Card>
         </Row>
       </Container>
     </div>
