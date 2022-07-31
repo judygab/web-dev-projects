7197ea876a2547fbdca9a7bd809bdd881d4fba13
judygab <ivditigabeskiria@gmail.com>
2022-07-31 12:25:23 -0400

    added button

---
+++ ecommerce-app-ui/components/elements/Button.jsx
@@ -0,0 +1,15 @@
+import clsx from 'clsx';
+
+export const Button = ({ children, onClick, variant="primary" }) => {
+
+    const defaultStyle = "h-10 px-5 m-2 rounded-lg";
+
+    const btnStyle = {
+        primary: "text-white bg-slate-800 hover:bg-slate-900",
+        secondary: "text-black bg-white hover:bg-slate-50"
+    }
+
+    return (
+        <button className={clsx(defaultStyle, btnStyle[variant])} onClick={onClick}>{children}</button>
+    )
+}
--- ecommerce-app-ui/pages/index.js
+++ ecommerce-app-ui/pages/index.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button } from '../components/elements/Button'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     <Button variant='primary'>this is btn</Button>
     </div>
   )
 }
