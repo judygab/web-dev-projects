a3b63658ad8c6bdd7e25d662caa976b0fee65ca2
judygab <ivditigabeskiria@gmail.com>
2022-08-10 00:26:42 +0400

    added search page

---
+++ ecommerce-app-ui/pages/search.js
@@ -0,0 +1,9 @@
+const SearchPage = () => {
+    return (
+        <div>
+            <h1 className="text-xl p-4 text-center font-graphikLight">Search</h1>
+        </div>
+    )
+}
+
+export default SearchPage;
