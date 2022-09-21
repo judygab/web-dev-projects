114b19ce99e11c05ddba9e124990cc7e531ff715
judygab <ivditigabeskiria@gmail.com>
2022-09-20 22:26:50 -0400

    added header link highlight

--- ecommerce-app-ui/components/navigation/Header.jsx
+++ ecommerce-app-ui/components/navigation/Header.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from "react";
 import Search from "../../assets/icons/search.svg";
 import User from "../../assets/icons/user.svg";
 import Cart from "../../assets/icons/cart.svg";
 import { NavButton } from "./NavButton";
 import { NavLink } from "./NavLink";
 import { useCart } from '../../context/cart-context';
+import { useRouter } from 'next/router';
 
 export const Header = () => {
     const {state} = useCart();
+    const [menuItemActive, setMenuItemActive] = useState('');
+
+    const router = useRouter()
+    const { categories, colors, gender, price } = router.query;
+
+    useEffect(() => {
+        if (gender !== undefined && !categories && !colors && !price) {
+            console.log("es")
+            setMenuItemActive(gender);
+        } else {
+            setMenuItemActive("");
+        }
+    }, [categories, colors, gender, price])
 
     return (
         <div className="flex flex-col items-center justify-center mt-32">
@@ -15,10 +30,16 @@ export const Header = () => {
                     backdrop-blur-md shadow-md w-full
                     fixed top-0 left-0 right-0 z-10">
                     <div className="navbar__menu-left flex h-100 items-center hidden space-x-8 lg:flex">
-                        <ul>
+                        <ul className="flex">
                             <NavLink 
-                                url="/women"
+                                url='/search?gender="Female"'
                                 label="Women"
+                                isActive={gender == `"Female"`}
+                                />
+                            <NavLink 
+                                url='/search?gender="Male"'
+                                label="Men"
+                                isActive={gender === `"Male"`}
                                 />
                         </ul>
                     </div>
--- ecommerce-app-ui/components/navigation/NavLink.jsx
+++ ecommerce-app-ui/components/navigation/NavLink.jsx
@@ -1,12 +1,15 @@
 import Link from 'next/link';
+import clsx from 'clsx';
 
-export const NavLink = ({ url, label }) => {
+export const NavLink = ({ url, label, isActive }) => {
+    const borderStyle = "border-b border-slate-900";
+
     return (
-        <li className="border-b border-slate-900 mr-1">
+        <li className={clsx(isActive && borderStyle, "mr-1")}>
             <Link href={url}>
-            <a className="flex text-gray-600 hover:bg-slate-200 cursor-pointer transition-colors duration-300 m-2 p-5 rounded-lg">
-                {label}
-            </a>
+                <a className="flex text-gray-600 hover:bg-slate-200 cursor-pointer transition-colors duration-300 m-2 p-5 rounded-lg">
+                    {label}
+                </a>
             </Link>
         </li>
     )
