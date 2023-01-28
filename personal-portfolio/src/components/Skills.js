6a4db1a9252949d3031ed80627515640c4727b53
Luka Brkovic <lucabrkovic@gmx.de>
2023-01-28 17:29:14 +0100

    circular progress bar dynamic

--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -1,13 +1,30 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
+import Pie from './Pie';
 
-export const Skills = () => {
+const skills = [
+  {
+    title: "Web Development",
+    prc: 80
+  },
+  {
+    title: "Brand Identity",
+    prc: 85
+  },
+  {
+    title: "Web Development",
+    prc: 85
+  }
+]
+
+
+
+
+ export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -23,11 +40,10 @@ export const Skills = () => {
       items: 2
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 768, min: 0 },
       items: 1
     }
   };
-
   return (
     <section className="skill" id="skills">
         <div className="container">
@@ -37,22 +53,16 @@ export const Skills = () => {
                         <h2>Skills</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
+                            
+                            {
+                              skills.map((item,idx) => (
+                                <div key={idx} className="item">
+                                <Pie prc={item.prc} title={item.title}/>
+
+                                </div>
+                              ))
+                            }
+                            
                         </Carousel>
                     </div>
                 </div>
