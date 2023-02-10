8cfe5e99e5acb2324c5aabbfafbb13cf92307795
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-02-10 16:24:37 -0300

    fix: change wrong class name

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -50,7 +50,7 @@ export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="aligh-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
