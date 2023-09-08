389838d41c347e0dca61c996f0e136d3051fa4f4
Yossijaki <yossijaki@gmail.com>
2023-09-08 14:19:33 -0600

    Update Projects.js
    This commit fixes the onClick function when clicking on "Projects" option in the NavBar. this is only a typo on the ID of the Projects component, adding the missing letter the onClick comes to work :)

--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -43,7 +43,7 @@ export const Projects = () => {
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col size={12}>
