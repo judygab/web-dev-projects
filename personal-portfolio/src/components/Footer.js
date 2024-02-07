b2240923f0edfe8e572a2d387f3556b9514bb3ce
Ali Asghar Ranjbar <72009720+AliAsgharRanjbar@users.noreply.github.com>
2024-02-07 23:47:25 +0330

    changed static years to be dynamic

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright {new Date().getFullYear()}. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
